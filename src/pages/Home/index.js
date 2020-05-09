b56e8fc5e1f93ad536203840e8aedb46410ee883
lucaspfeliciano <lucaspfeliciano@hotmail.com>
2020-05-08 21:24:31 -0300

    Estiliza pagina Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,84 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md'
+
+import { ProductList } from './styles';
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <ProductList>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="https://static.netshoes.com.br/produtos/tenis-adidas-run-falcon-feminino/12/NQQ-0553-012/NQQ-0553-012_zoom2.jpg?ts=1587049970&ims=326x" alt="Tenis Adidas"/>
+                <strong>Tenis Adidas T50</strong>
+                <span>R$129,90</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size= {16} color="#FFF"></MdAddShoppingCart> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+        </ProductList>
     )
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -14,7 +14,7 @@ export default createGlobalStyle`
 }
 
 body {
-    background: #191920 url(${background}) no-repeat center top;
+    background: #D3D3D3 url(${background}) no-repeat center top;
     -webkit-font-smoothing: antialiased;
 }
 
